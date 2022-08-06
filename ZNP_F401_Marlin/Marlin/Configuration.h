65d2cfaa82e76f4b52c664fce2749f18b2622b06
NARUTOfzr <448348416@qq.com>
2022-08-06 14:33:48 +0800

    Avoid Z-axis drop

--- ZNP_F401_Marlin/Marlin/Configuration.h
+++ ZNP_F401_Marlin/Marlin/Configuration.h
@@ -1359,9 +1359,9 @@
  *  - Use a low value (i.e., Z_MIN_POS) if the nozzle falls down to the bed.
  *  - Use a large value (i.e., Z_MAX_POS) if the bed falls down, away from the nozzle.
  */
-//#define Z_IDLE_HEIGHT Z_HOME_POS
+#define Z_IDLE_HEIGHT Z_HOME_POS
 
-#define Z_HOMING_HEIGHT  4      // (mm) Minimal Z height before homing (G28) for Z clearance above the bed, clamps, ...
+#define Z_HOMING_HEIGHT    8    // (mm) Minimal Z height before homing (G28) for Z clearance above the bed, clamps, ...
                                   // Be sure to have this much clearance over your Z_MAX_POS to prevent grinding.
 
 #define Z_AFTER_HOMING  0.2      // (mm) Height to move to after homing Z
