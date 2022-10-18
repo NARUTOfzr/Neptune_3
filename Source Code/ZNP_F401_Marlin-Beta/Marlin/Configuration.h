b926e00c3af6a7a4e64655bc6773f67d94b00f92
David McKenna <dmckenna@cp.dias.ie>
2022-10-18 20:54:47 +0100

    Better bed PID tuning (from M303 E-1 C10 S60)

--- Source Code/ZNP_F401_Marlin-Beta/Marlin/Configuration.h
+++ Source Code/ZNP_F401_Marlin-Beta/Marlin/Configuration.h
@@ -645,9 +645,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 106.23
+  #define DEFAULT_bedKi 20.75
+  #define DEFAULT_bedKd 362.61
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
